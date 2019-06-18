a6ae7946787ea912e3244a4856c06f96a3db02ef
DDulionaire <tigk0070@gmail.com>
2019-06-18 10:26:13 -0700

    Details and CSS added

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,7 +1,8 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: "https://graphmovie-o48hizn9n.now.sh/"
+  uri: "https://graphmovie-cfrkkn0r5.now.sh/"
+  //uri: "https://graphmovie-a8938hdz9.now.sh/"
 });
 
 export default client;
